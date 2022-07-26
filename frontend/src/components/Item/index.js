cb1ce969dbddfb274221212b00255c5f60aad8d1
josh kennedy <josh@outthinkgroup.com>
2022-07-26 09:44:16 -0400

    adds a placeholder image to the item

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -37,7 +37,6 @@ class Item extends React.Component {
     if (!this.props.item) {
       return null;
     }
-
     const markup = {
       __html: marked(this.props.item.description, { sanitize: true }),
     };
@@ -50,13 +49,12 @@ class Item extends React.Component {
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image}
+                src={this.props.item.image || `/placeholder.png`}
                 alt={this.props.item.title}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
               />
             </div>
-
             <div className="col-6">
               <h1>{this.props.item.title}</h1>
               <ItemMeta item={this.props.item} canModify={canModify} />
